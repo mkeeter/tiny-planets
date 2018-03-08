92623ef7fe9bc41533fcf4e1543d9dafff4e1e01
Matt Keeter <matt.j.keeter@gmail.com>
2018-03-07 20:05:36 -0500

    Add signal handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 extern crate glium;
 extern crate libloading;
+extern crate ctrlc;
 
 use std::fs;
 use std::time;
 use std::process::Command;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
 use libloading::{Library, Symbol};
 
 struct Handle {
@@ -101,13 +105,18 @@ impl Drop for Handle {
 fn main() {
     use glium::glutin;
 
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
+
     let mut events_loop = glutin::EventsLoop::new();
     let window = glutin::WindowBuilder::new();
     let context = glutin::ContextBuilder::new();
     let display = glium::Display::new(window, context, &events_loop).unwrap();
 
     let mut counter = 0;
-    let mut closed = false;
 
     // Run 'cargo watch' to rebuild libraries in the background
     let mut child = Command::new("cargo")
@@ -116,7 +125,7 @@ fn main() {
                            .expect("Failed to start 'cargo watch'");
 
     let mut handle = Handle::new("target/debug/liblive.dylib".to_string());
-    while !closed {
+    while running.load(Ordering::SeqCst) {
         handle.check();
 
         handle.draw(counter, display.draw());
@@ -125,7 +134,7 @@ fn main() {
         events_loop.poll_events(|ev| {
             match ev {
                 glutin::Event::WindowEvent { event, .. } => match event {
-                    glutin::WindowEvent::Closed => closed = true,
+                    glutin::WindowEvent::Closed => running.store(false, Ordering::SeqCst),
                     _ => (),
                 },
                 _ => (),
